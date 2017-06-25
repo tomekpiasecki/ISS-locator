e57a3be32c6182390b434cff1af819848e2539fe
Tomek Piasecki <netpiaseq+github@gmail.com>
2017-06-25 17:53:14 +0200

    Basic routing with nikic/fast-route and a dummy controller

--- pub/index.php
+++ pub/index.php
@@ -2,8 +2,35 @@
 
 declare(strict_types = 1);
 
+use FastRoute\Dispatcher;
+use Isslocator\Controller\IndexAction;
+
 define('DS', DIRECTORY_SEPARATOR);
 
 require __DIR__ . DS . '..' . DS . 'app' . DS . 'boostrap.php';
 
-echo 'it works';
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', [IndexAction::class, 'execute']);
+});
+
+$routeInfo = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+switch ($routeInfo[0]) {
+    case Dispatcher::NOT_FOUND:
+        header("HTTP/1.1 404 Not Found");
+        echo '<h1>Not found</h1>';
+        break;
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        header("HTTP/1.1 405 Method Not Allowed");
+        echo '<h1>Method not allowed</h1>';
+        break;
+    case Dispatcher::FOUND:
+        $className = $routeInfo[1][0];
+        $method = $routeInfo[1][1];
+        $parameters = $routeInfo[2];
+
+        $class = new $className;
+        $response = $class->$method($parameters);
+        header("HTTP/1.1 200 OK");
+        echo $response;
+        break;
+}
---
+++ src/Controller/IndexAction.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Isslocator\Controller;
+
+class IndexAction
+{
+    public function execute()
+    {
+        return __METHOD__;
+    }
+}
