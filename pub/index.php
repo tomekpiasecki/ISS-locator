0c9a1618cf872359d52eb71380950a70009845ac
Tomek Piasecki <netpiaseq+github@gmail.com>
2017-06-26 19:35:55 +0200

    Global error handler

--- pub/index.php
+++ pub/index.php
@@ -22,27 +22,32 @@ $request = $diContainer->make(Request::class);
 /** @var Response $response */
 $response = $diContainer->make(Response::class);
 
-$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getRequestUri());
-switch ($routeInfo[0]) {
-    case Dispatcher::NOT_FOUND:
-        $response->setStatusCode(404);
-        $response->setContent('<h1>Not found</h1>');
-        break;
-    case Dispatcher::METHOD_NOT_ALLOWED:
-        $response->setStatusCode(405);
-        $response->setContent('<h1>Method not allowed</h1>');
-        break;
-    case Dispatcher::FOUND:
-        $className = $routeInfo[1][0];
-        $method = $routeInfo[1][1];
-        $parameters = $routeInfo[2];
+try {
+    $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getRequestUri());
+    switch ($routeInfo[0]) {
+        case Dispatcher::NOT_FOUND:
+            $response->setStatusCode(404);
+            $response->setContent('<h1>Not found</h1>');
+            break;
+        case Dispatcher::METHOD_NOT_ALLOWED:
+            $response->setStatusCode(405);
+            $response->setContent('<h1>Method not allowed</h1>');
+            break;
+        case Dispatcher::FOUND:
+            $className = $routeInfo[1][0];
+            $method = $routeInfo[1][1];
+            $parameters = $routeInfo[2];
 
-        $class = $diContainer->make($className);
-        $content = $class->$method($parameters);
+            $class = $diContainer->make($className);
+            $content = $class->$method($parameters);
 
-        $response->setStatusCode(200);
-        $response->setContent($content);
-        break;
+            $response->setStatusCode(200);
+            $response->setContent($content);
+            break;
+    }
+} catch (\Throwable $ex) {
+    $response->setStatusCode(500);
+    $response->setContent(@file_get_contents(__DIR__ . DS . '..' . DS . 'templates' . DS . '500.html'));
 }
 
 $response->prepare($request);
