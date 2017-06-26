bf8a8cd99c9b59ce019377617bc9362d6f2da0ca
Tomek Piasecki <netpiaseq+github@gmail.com>
2017-06-26 17:45:32 +0200

    Methods to walidate and decode response added to Guzzle client adapter

--- src/Http/GuzzleClient.php
+++ src/Http/GuzzleClient.php
@@ -11,7 +11,9 @@ class GuzzleClient implements Client
     /**
      * @var GuzzleHttpClient
      */
-    private $guzzleClient;
+    protected $guzzleClient;
+
+    protected $response;
 
     public function __construct(GuzzleHttpClient $guzzleClient)
     {
@@ -21,8 +23,38 @@ class GuzzleClient implements Client
     /**
      * @inheritdoc
      */
-    public function request(string $method, string $uri = '', array $options = [])
+    public function request(string $method, string $uri = '', array $options = []):array
     {
-        return $this->guzzleClient->request($method, $uri, $options);
+        $this->response = $this->guzzleClient->request($method, $uri, $options);
+        if (!$this->wasRequestSuccessful()) {
+            throw new \Exception ('request failed');
+        }
+
+        $this->decodeResponse();
+
+        return $this->response;
+    }
+
+    /**
+     * Validates the response status code
+     *
+     * @return bool
+     */
+    protected function wasRequestSuccessful():bool
+    {
+        return $this->response->getStatusCode() === Client::RESPONSE_CODE_SUCCESS;
+    }
+
+    /**
+     * Decodes JSON response to array
+     *
+     * @throws \Exception
+     */
+    protected function decodeResponse()
+    {
+        $this->response = json_decode((string)$this->response->getBody(), true);
+        if (json_last_error()) {
+            throw new \Exception ('request failed');
+        }
     }
 }
